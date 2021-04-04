17eb0b8500d2c1068134853328dbcaeed4b3c4f6
Tharun <reddytharun9495@gmail.com>
2021-04-04 22:45:21 +0530

    first commit

---
+++ js_files/exxx.js
@@ -0,0 +1,60 @@
+// console.log("today is'wednesday'")
+// console.log("today is"wednesday"")
+// console.log("today is'wednesday'")
+// console.log('today is'wednesday'')
+
+
+
+
+//var date = 15
+// if(date===07){
+//     document.write("rose day")
+// }
+// else if(date===08){
+//     document.write("propose day")
+// }
+// else if(date===09){
+//     document.write("chocolate day")
+// }
+// else if(date===10){
+//     document.write("teddy day")
+// }
+// else if(date===11){
+//     document.write("promise day")
+// }
+// else if(date===12){
+//     document.write("kiss day")
+// }
+// else if(date===13){
+//     document.write("hug day")
+// }
+// else if(date===14){
+//     document.write("valentines day")
+// }
+// else{
+//     document.write("enter the date of a valentine week")
+// }
+
+
+
+
+// a=7
+// if (a%2 === 0)
+// {
+//     document.write(a +":it is even number")
+// }
+// else{
+//     document.write(a +": is odd number")
+// }
+
+
+// WAp for displaying status based on proposal
+var statusOfProposal="rejected"
+switch (statusOfProposal) {
+    case "accepted":
+        console.log("Do party")
+        break;
+    case "rejected":
+        console.log("join pysiders")
+        break;
+}
